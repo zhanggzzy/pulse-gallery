f58c301be3516ff3826a3e1fd170ae12b8634685
Otto <zzy801997@163.com>
2025-06-17 00:31:20 +0800

    frontend code, project MVP done

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ImageCard from './components/ImageCard'
+
+
+function App() {
+  return (
+    <div className="w-full h-full">
+      <ImageCard />
+    </div>
+  )
+}
+
+export default App
