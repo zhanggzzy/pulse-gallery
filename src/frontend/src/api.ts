a12bf30ebcf9c3138e8aa364da1ef7dc1c9e03c0
Otto <zzy801997@163.com>
2025-06-22 20:49:53 +0800

    replase front end api with relative path

--- src/frontend/src/api.ts
+++ src/frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = "http://localhost:8000/api" // replace with .env later
+const API_BASE = "/api"
 
 export const fetchRandomImage = async () => {
     const res = await axios.get(`${API_BASE}/image/random`)
